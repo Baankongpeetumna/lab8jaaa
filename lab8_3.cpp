35f18c6df86a5c41bc96007b1eb5b5122852d6da
Benyaporn <Baibua41430@gmail.com>
2024-12-09 15:32:23 +0700

    lab8_3_completeja

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,36 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A,H,B;
+    cout << "Enter your age: " ;
+    cin >> A;
+    if ( A <= 25) { 
+        cout << "Enter your height: " ;
+        cin >> H;
+        if (H < 100){
+        cout << "Your character = Chopper";}
+        else if (H < 180){
+        cout << "Your character = Usopp";
+        } else {
+        cout << "Enter your bounty: ";
+        cin >> B;
+        if (B > 1100000000 ) {
+        cout << "Your character = Zoro";
+        } else {
+        cout << "Your character = Sanji";}
+            }
+    } else if (A <= 60 ) {
+        cout << "Enter your bounty: " ;
+        cin >> B;
+        if (B > 500000000) {
+            cout << "Your character = Jinbe";
+        } else {
+            cout << "Your character = Franky";
+        }
+    } else {
+        cout << "Your character = Brook";
+    }
+        
+    
+}
